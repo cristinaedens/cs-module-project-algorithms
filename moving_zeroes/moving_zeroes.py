f5f6f3dabadb59557debea81dec470b8b6543120
Cristina Edens <edens.cristina@gmail.com>
2020-07-15 13:55:44 -0400

    moving_zeroes passing

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,8 +4,20 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    
+    # finds position in list
+    index = 0
+    for i in range(len(arr)):
+        # moves up in list
+        if arr[i] != 0:
+            arr[index]=arr[i]
+            index+=1
+    #replaces ending with 0's
+    for i in range(index,len(arr)):
+        arr[i]=0
+    return arr
 
-    pass
+   
 
 
 if __name__ == '__main__':
