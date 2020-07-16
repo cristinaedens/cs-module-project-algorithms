e1d2c7642c528708e92d113d02f131abbeee3ed3
Cristina Edens <edens.cristina@gmail.com>
2020-07-16 15:15:30 -0400

    eating cookies optomized passing

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,43 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, cache=None):
     # Your code here
+#     if n <= 0:
+#         return 1
+#     if n is 1:
+#         return 1
+#     if n is 2:
+#         return 2
+#     if n is 3:
+#         return 4
+#     if n in results.keys():
+#         return results[n]
+#     one_cookie = eating_cookies(n - 1)
+#     two_cookies = eating_cookies(n - 2)
+#     three_cookies = eating_cookies(n - 3)
+#     if n not in results.keys():
+#         results[n] = one_cookie + two_cookies + three_cookies
+#         return one_cookie + two_cookies + three_cookies
 
-    pass
+    if n < 0:
+        return 0
+    if n is 0:
+        return 1
+    if cache is None:
+        cache = {}
+    elif isinstance(cache, list):
+        cache = dict.fromkeys(cache)
+    if n in cache:
+        return cache[n]
+    one_cookie = eating_cookies(n - 1, cache)
+    two_cookies = eating_cookies(n - 2, cache)
+    three_cookies = eating_cookies(n - 3, cache)
+
+    cache[n] = one_cookie + two_cookies + three_cookies
+
+    return cache[n]
+    
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
